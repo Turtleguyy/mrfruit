f7228eb8d7f57abeea33597992ba8710a15cbea7
Zachary Nicoll <git@zdn.me>
2019-12-03 23:51:36 -0600

    Initial setup

--- config/environment.js
+++ config/environment.js
@@ -3,6 +3,7 @@
 module.exports = function(environment) {
   let ENV = {
     modulePrefix: 'mrfruit',
+    podModulePrefix: 'mrfruit/pods',
     environment,
     rootURL: '/',
     locationType: 'auto',
